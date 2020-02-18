c69c84f509865d04e7caf4c12a4a38a57690c96a
unknown <Administrator@SILAB-41.ASIETDomain.com>
2020-02-18 12:30:13 +0530

    product array

--- src/App.js
+++ src/App.js
@@ -1,20 +1,42 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import tiles from './components/tiles';
 
+const productsAr=[
+  {
+    "id":1,
+     "title":"Realme 5i (Forest Green, 64 GB)  (4 GB RAM)",
+     "price":"₹8,999",
+     "image":"https://rukminim1.flixcart.com/image/312/312/k612pow0/mobile/b/s/r/realme-c3-rmx2027-original-imafzkzhts5kfmxt.jpeg?q=70"
+  },
+  {
+    "id":2,
+     "title":"Realme 5i (Aqua Blue, 64 GB)  (4 GB RAM)",
+     "price":"₹8,999",
+     "image":"https://rukminim1.flixcart.com/image/416/416/k65d18w0pkrrdj/mobile/r/x/z/realme-5i-rmx2030-original-imafnsx5pakdfdpb.jpeg?q=70"
+  },
+  {
+    "id":3,
+     "title":"Redmi 7A (Matte Black, 32 GB)  (2 GB RAM)",
+     "price":"₹5,499",
+     "image":"https://rukminim1.flixcart.com/image/416/416/jzrb53k0pkrrdj/mobile/y/c/h/mi-redmi-7a-mzb7798in-original-imafg27hkgpkhsdy.jpeg?q=70"
+  },
+]
 class App extends React.Component {
    constructor(props){
      super(props);
      this.state={
        firstState: 'Welcome',
-       alertcartCount:0
-       
+      cartCount:0,
+       product:[]
      }
    }
 
   componentDidMount() {
     this.setState({
-      firstState: 'The biggest choice'
+      firstState:'The biggest choice',
+    
     })
   }
  
@@ -26,46 +48,33 @@ class App extends React.Component {
   }
   componentDidUpdate(prevProps,prevState){
     if(this.state.cartCount===10)
-       alertcartCount('Not possible')
+      alert('Not possible')
+  }
+  loadproducts(){
+    this.setState({
+      product:productsAr
+    })
   }
   render() {
+    const productslist=this.state.product.map((prod)=>{
+       return tiles(this.addCount.bind(this),prod)
+    })
+    console.log(productslist)
     return (
       <div>
         <nav className="navbar navbar-expand-lg navbar-light bg-light d-flex justify-content-between">
           <a className="navbar-brand" href="#">Navbar</a>
-          {this.state.firstState}
-         
+          <button onClick={()=>{this.loadproducts()}}>load products</button>
           <button className="right" type="button">
             Cart ({this.state.cartCount})
-        </button>
-        
+        </button>        
         </nav>
         <div className="container">
           <div className="row">
             <h2>Products</h2>
           </div>
           <div className="row">
-            <div className="col-md-4 col-lg-3 col-sm-12">
-              <div className="card mb-3" style={{"maxWidth": "540px", "color": "red"}}>
-                <div className="row no-gutters">
-                  <div className="col-md-4">
-                    <img src="https://via.placeholder.com/150" className="card-img" width="100%" height="100%" />
-                  </div>
-                  <div className="col-md-8">
-                    <div className="card-body">
-                      <h5 className="card-title" id="#first-title">Product title</h5>
-                      <div>
-                        <button className="btn btn-primary add-btn"
-                        onClick={()=>{
-                           this.addCount()
-                        }}>add</button>
-                       
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
+            {productslist}
           </div>
           <div className="row d-flex justify-content-end">
             <button className="btn btn-primary" id="checkout-btn">Checkout</button>
---
+++ src/components/tiles.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+export default function tiles(addCount,prod)
+{
+  return(
+    <div className="col-md-4 col-lg-3 col-sm-12">
+              <div className="card mb-3" style={{"maxWidth": "540px", "color": "red"}}>
+                <div className="row no-gutters">
+                  <div className="col-md-4">
+                    <img src={prod.image} className="card-img" width="100%" height="100%" />
+                  </div>
+                  <div className="col-md-8">
+                    <div className="card-body"style={{"maxWidth": "540px", "color": "black"}}>
+                      <h5 className="card-title" id="#first-title"style={{"maxWidth": "540px", "color": "black"}}>{prod.title}</h5>
+                      <h6 className="card-title" id="#first-title"style={{"maxWidth": "540px", "color": "red"}}>{prod.price}</h6>
+                      <div>
+                        <button className="btn btn-primary add-btn"
+                        onClick={()=>{addCount()}
+                         }
+                    >add</button>
+                       
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+  )
+}
